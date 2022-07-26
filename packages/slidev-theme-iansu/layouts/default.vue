e9f8e86ca14f177557f3a308a8c6b3e2264fb289
Ian Sutherland <ian@iansutherland.ca>
2022-07-25 23:13:28 -0600

    WIP extended slides

--- packages/slidev-theme-iansu/layouts/default.vue
+++ packages/slidev-theme-iansu/layouts/default.vue
@@ -3,5 +3,8 @@
     <div class="self-center">
       <slot />
     </div>
+    <div class="absolute bottom-4 right-4 text-opacity-50">
+      <img src="../images/is-logo-black.png" width="40" />
+    </div>
   </div>
 </template>

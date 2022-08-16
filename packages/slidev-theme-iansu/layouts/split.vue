21e8f203b3bc5775b6d39fff907176f2a5805cc7
Ian Sutherland <ian@iansutherland.ca>
2022-08-16 15:09:30 -0600

    Zero Dependency CLIs extended version (#1)

--- packages/slidev-theme-iansu/layouts/split.vue
+++ packages/slidev-theme-iansu/layouts/split.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="split">
-    <slot />
+  <div>
+    <div class="split">
+      <slot />
+    </div>
+    <div class="absolute bottom-4 right-4">
+      <img src="../images/is-logo-black.png" width="40" />
+    </div>
   </div>
 </template>
 
